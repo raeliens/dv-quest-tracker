74fbd03e2075081c53c79aae0cca4ec8fe128627
Diem-Thy Barsell <dfbarsell@gmail.com>
2018-03-15 09:20:57 -0400

    nevermind

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-$(document).ready(function() {
-
 var count = 1;
 document.querySelectorAll('button').forEach(function(a) {
   a.addEventListener('click', setColor)
@@ -11,4 +9,3 @@ function setColor() {
   this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
   this.style.backgroundColor = col[this.dataset.count % col.length]
 }
-});
