08051011ef41ef47c2749e17954c6057b522c725
Diem-Thy Barsell <dfbarsell@gmail.com>
2018-03-15 08:43:32 -0400

    create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+var count = 1;
+document.querySelectorAll('button').forEach(function(a) {
+  a.addEventListener('click', setColor)
+})
+
+var col = ["#fffc59", "#5afc8e"];
+
+function setColor() {
+  this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
+  this.style.backgroundColor = col[this.dataset.count % col.length]
+}
