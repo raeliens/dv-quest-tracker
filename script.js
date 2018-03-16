47d01b08a9a7235bb1902183e820608e2a522aff
rae <23226442+raeliens@users.noreply.github.com>
2018-03-16 06:43:54 -0400

    Update script.js
    wtf i broke it

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
   $(document).ready(function() {
-  var count = 1;
-  document.querySelectorAll('button').forEach(function(a) {
-    a.addEventListener('click', setColor)
-  })
+    var count = 1;
+    document.querySelectorAll('button').forEach(function(a) {
+      a.addEventListener('click', setColor)
+    })
 
-  var col = ["#fffc59", "#5afc8e", "#ff5959"];
+    var col = ["#fffc59", "#5afc8e", "#ff5959"];
 
-  function setColor() {
-    this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
-    this.style.backgroundColor = col[this.dataset.count % col.length]
-  }
-}, false);
+    function setColor() {
+      this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
+      this.style.backgroundColor = col[this.dataset.count % col.length]
+    }
+  }, false);
 
-    $(color).click(function() {
-      $('h2').toggleClass('dark');
+  $(color).click(function() {
+    $('h2').toggleClass('dark');
   });
