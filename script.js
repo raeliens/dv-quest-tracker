b0e0b4a500b29d880d6db955ddd36f6c64945921
raeliens <23226442+raeliens@users.noreply.github.com>
2018-03-16 00:35:17 -0400

    Update script.js
    now shes a mess of pure js and jQ but idc shes my daughter n i love her

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-window.addEventListener('load', function () {
-var count = 1;
-document.querySelectorAll('button').forEach(function(a) {
-  a.addEventListener('click', setColor)
-})
+window.addEventListener('load', function() {
+  var count = 1;
+  document.querySelectorAll('button').forEach(function(a) {
+    a.addEventListener('click', setColor)
+  })
 
-var col = ["#fffc59", "#5afc8e", "#ff5959"];
+  var col = ["#fffc59", "#5afc8e", "#ff5959"];
 
-function setColor() {
-  this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
-  this.style.backgroundColor = col[this.dataset.count % col.length]
-}
+  function setColor() {
+    this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
+    this.style.backgroundColor = col[this.dataset.count % col.length]
+  }
+
+// sorry i dont know how to code this in pure js
+// instead ive just coded this part in jQuery
+// maybe one day we can clean this up & make her toggle
+
+$(document).ready(function() {
+    $(color).click(function() {
+        $('h2').attr('style','color: black;');
+        $(color).attr('style','color: black;');
+    });
+});
 }, false);
-
-
-document.getElementById('change').onclick = changeColor;   
-
-    function changeColor() {
-        document.h3.style.color = "black";
-        return false;
-    }   
