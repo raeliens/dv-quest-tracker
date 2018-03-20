3f11c00b3052588a8c9423ec25d6f8a209e7025d
rae <23226442+raeliens@users.noreply.github.com>
2018-03-20 06:21:53 -0400

    sdfdf

--- script.js
+++ script.js
@@ -2,29 +2,30 @@
 //                     ENDARKEN FONT!                     //
 ////////////////////////////////////////////////////////////
 
+
 $(document).ready(function () {
-     $(color).click(function () {
-         $('h2').toggleClass('dark');
-     });
- });
+    $(color).click(function () {
+        $('h2').toggleClass('dark');
+    });
+});
 
 
 ////////////////////////////////////////////////////////////
 //                 BUTTON COLOR CYCLES                    //
 ////////////////////////////////////////////////////////////
 
- var colors = ["#fffc59", "#5afc8e", "#ff5959"];
+var colors = ["#fffc59", "#5afc8e", "#ff5959"];
 
- function setColor(el) {
-     el.colorIdx = el.colorIdx || 0;
-     el.style.backgroundColor = colors[el.colorIdx++ % colors.length];
- }
+function setColor(el) {
+    el.colorIdx = el.colorIdx || 0;
+    el.style.backgroundColor = colors[el.colorIdx++ % colors.length];
+}
 
 
- //      $('#reset').click(function () {
- //          $('button').css('background-color', colors[2]);
- //      })
- //  });
- // To add reset button
- // use following in HTML:
- // <button id="reset" title="Reset progress">Reset</button>
+//      $('#reset').click(function () {
+//          $('button').css('background-color', colors[2]);
+//      })
+//  });
+// To add reset button
+// use following in HTML:
+// <button id="reset" title="Reset progress">Reset</button>
