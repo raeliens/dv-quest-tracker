a1f8fa3cf4bed450bef5c6aed1cf8f2127961bf3
rae <23226442+raeliens@users.noreply.github.com>
2018-03-19 21:58:22 -0400

    Updates
    Changed method of setting button colors, precursor to saving the button color state to localStorage

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
   $(document).ready(function() {
-  var count = 1;
-  document.querySelectorAll('button').forEach(function(a) {
-    a.addEventListener('click', setColor)
-  })
-
-  var col = ["#fffc59", "#5afc8e", "#ff5959"];
-
-  function setColor() {
-    this.dataset.count = this.dataset.count >= 0 ? ++this.dataset.count : 0;
-    this.style.backgroundColor = col[this.dataset.count % col.length]
-  }
-}, false);
-
- 
-  $(document).ready(function() {
     $(color).click(function() {
       $('h2').toggleClass('dark');
     });
   });
+
+var colors = ["#fffc59", "#5afc8e", "#ff5959"];
+
+function setColor(el) {
+    el.colorIdx = el.colorIdx || 0;
+    el.style.backgroundColor = colors[el.colorIdx++ % colors.length];
+}
