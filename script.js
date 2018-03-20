1c4ff625abf40f4aeb987e70de6afd81857a6102
rae <23226442+raeliens@users.noreply.github.com>
2018-03-20 06:20:41 -0400

    :+(
    im really sick of coding now lmao

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-  $(document).ready(function () {
-      $(color).click(function () {
-          $('h2').toggleClass('dark');
-      });
-  });
+////////////////////////////////////////////////////////////
+//                     ENDARKEN FONT!                     //
+////////////////////////////////////////////////////////////
 
-  var colors = ["#fffc59", "#5afc8e", "#ff5959"];
-
-  function setColor(el) {
-      el.colorIdx = el.colorIdx || 0;
-      el.style.backgroundColor = colors[el.colorIdx++ % colors.length];
-  }
+$(document).ready(function () {
+     $(color).click(function () {
+         $('h2').toggleClass('dark');
+     });
+ });
 
 
-  //      $('#reset').click(function () {
-  //          $('button').css('background-color', colors[2]);
-  //      })
-  //  });
-  // To add reset button
-  // use following in HTML:
-  // <button id="reset" title="Reset progress">Reset</button>
+////////////////////////////////////////////////////////////
+//                 BUTTON COLOR CYCLES                    //
+////////////////////////////////////////////////////////////
+
+ var colors = ["#fffc59", "#5afc8e", "#ff5959"];
+
+ function setColor(el) {
+     el.colorIdx = el.colorIdx || 0;
+     el.style.backgroundColor = colors[el.colorIdx++ % colors.length];
+ }
+
+
+ //      $('#reset').click(function () {
+ //          $('button').css('background-color', colors[2]);
+ //      })
+ //  });
+ // To add reset button
+ // use following in HTML:
+ // <button id="reset" title="Reset progress">Reset</button>
